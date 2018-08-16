4ebc570e00c81f26095aa9f69f1cec5fa90f5cc2
zankoav <zankoav@mail.ru>
2018-08-16 13:53:18 +0300

    initial project

---
+++ 404.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found)
+ *
+ */
+
---
+++ front-page.php
@@ -0,0 +1,7 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: alexandrzanko
+	 * Date: 8/16/18
+	 * Time: 11:24 AM
+	 */
