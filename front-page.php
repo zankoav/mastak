cde220601d256b35b7059019494e43b356e5f770
zankoav <zankoav@mail.ru>
2018-08-20 17:58:56 +0300

    remove email

--- front-page.php
+++ front-page.php
@@ -9,13 +9,12 @@
             <img class="tiser__logo-img" src="/wp-content/themes/mastak/src/icons/MastakAnime-BW.8a3f31.gif"
                  alt="mastakstudio">
         </a>
-        <a class="tiser__email tiser__email_after-logo" href="mailto:<?= $email; ?>"><?= $email; ?></a>
     </div>
     <div class="tiser__content">
         <h1 class="tiser__title tiser__title_center">coming soon…</h1>
         <p class="tiser__description tiser__description_center">The website is under construction. We’ll be here soon
             with our new awesome site, subscribe to be notified.</p>
-		<?php echo do_shortcode( '[contact-form-7 id="5" title="Subscribe" html_class="tiser__form tiser__form_main"]' ); ?>
+		<?php echo do_shortcode( '[contact-form-7 id="4" title="Subscribe" html_class="tiser__form tiser__form_main"]' ); ?>
         <a class="tiser__download-price tiser__download-price_red" href="<?= $presentation; ?>">
             <img class="tiser__download-icon" src="/wp-content/themes/mastak/src/icons/download.0460dd.svg"
                  alt="download">
