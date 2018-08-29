d2674d181833a10340550ae9d3c7b205a0be05fa
Ctrelka <eduard.ignatyukl.1998@mail.ru>
2018-08-29 11:22:43 +0300

    create partners swiper

---
+++ assets/components/partners/partners.js
@@ -0,0 +1,32 @@
+import './partners.scss';
+import Swiper from 'swiper';
+
+let swiper = new Swiper('.swiper-container', {
+    slidesPerView: 7,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 1000,
+        stopOnLast: true,
+        disableOnInteraction: false
+    },
+    breakpoints: {
+        480: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+        },
+        640: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+        768: {
+            slidesPerView: 5,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 7,
+            spaceBetween: 40,
+        },
+    }
+});
+
--- assets/templates/home.js
+++ assets/templates/home.js
@@ -1,3 +1,5 @@
 import './../components/_base.scss';
+import './../components/_swiper.scss';
 import './../components/services/services.js';
+import './../components/partners/partners.js';
 import './../components/copyright/copyright.js';
