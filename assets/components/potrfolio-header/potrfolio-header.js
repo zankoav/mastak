d4a7f935dc2a10be7d37ce6ece60c4baa12fd44d
Ctrelka <eduard.ignatyukl.1998@mail.ru>
2018-09-13 19:19:59 +0300

    create header component

---
+++ assets/components/potrfolio-header/potrfolio-header.js
@@ -0,0 +1,1 @@
+import './potrfolio-header.scss';
---
+++ assets/templates/portfolio.js
@@ -0,0 +1,4 @@
+import './../components/_base.scss';
+import './../components/menu/menu';
+import './../components/potrfolio-header/potrfolio-header';
+import './../components/copyright/copyright';
