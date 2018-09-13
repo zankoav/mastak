b8159b1f5b449be0c7bd3075773aa51ca9b09450
zankoav <zankoav@mail.ru>
2018-09-13 17:57:51 +0300

    add menu

--- assets/components/menu/menu.js
+++ assets/components/menu/menu.js
@@ -1,8 +1,17 @@
 import './menu.scss';
 import $ from 'jquery';
+import {mainSwiper} from "./../main_swiper/main_swiper";
 
 $('.header__burger').click(function (event) {
     event.preventDefault();
     $(this).toggleClass('header__burger_active');
     $('.menu').toggleClass('menu_active');
+
+    if(mainSwiper.realIndex === 1){
+        $('.header__line-burger').toggleClass('header__line-burger_black');
+    }
+
+    if(screen.width >= 768){
+        $('.portfolio__menu-slider').fadeToggle();
+    }
 });
