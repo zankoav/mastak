4648566c470bff4e898b5a9e2c165b465d84e646
Ctrelka <eduard.ignatyukl.1998@mail.ru>
2018-09-24 15:33:51 +0300

    create portfolios page

--- assets/templates/portfolio.js
+++ assets/templates/portfolio.js
@@ -2,4 +2,6 @@ import './../components/_base.scss';
 import './../components/menu/menu';
 import './../components/potrfolio-header/potrfolio-header';
 import './../components/menu-projects/menu-projects';
+import './../components/portfolios/portfolios';
+import './../components/portfolio-card/portfolio-card';
 import './../components/copyright/copyright';
